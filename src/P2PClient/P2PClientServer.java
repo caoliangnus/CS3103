1d5e56feacd19fb54344a1a573b2bf3a00e892f8
jas5469 <jas_p90@hotmail.com>
2018-10-12 22:15:34 +0800

    udpated

--- src/P2PClient/P2PClientServer.java
+++ src/P2PClient/P2PClientServer.java
@@ -10,25 +10,36 @@ public class P2PClientServer extends Thread {
     private void handleIncomingConnections()  {
         // Need to take in a filename and a chunk number and then start sending.
         String fileRequested;
-        try{
+        String clientAddress;
+        String fileName;
+        String chunkNumber;
+        try {
             clientServerSocket = new Socket(InetAddress.getLocalHost(), SERVER_PORT);
-            while(true) {
+            while (true) {
                 BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientServerSocket.getInputStream()));
                 fileRequested = inFromClient.readLine();
-                
+                //need to split string and chunk number
+                clientAddress=fileRequested;
+                fileName = fileRequested;
+                chunkNumber = fileRequested;
+                sendFile(clientAddress, fileName, chunkNumber);
 
             }
-
         } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(e);
-            System.exit(1);
-        }
+        e.printStackTrace();
+        System.out.println(e);
+        System.exit(1);
+    }
+
 
     }
 
 
     public void start() {
         handleIncomingConnections();
+    }
+
+    public void sendFile(String clientAddress, String fileName, String chunkNumber){
+
     }
 }
