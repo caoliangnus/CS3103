6faf1161c71600ae49655b3b1cd212541e0e22b4
jas5469 <jas_p90@hotmail.com>
2018-10-12 22:07:34 +0800

    working on P2P server handle connection

--- src/P2PClient/P2PClientServer.java
+++ src/P2PClient/P2PClientServer.java
@@ -1,10 +1,32 @@
 package P2PClient;
+import java.io.*;
+import java.net.*;
 
 public class P2PClientServer extends Thread {
 
-    private void handleIncomingConnections() {
+    public static final int SERVER_PORT = 8888;
+    private Socket clientServerSocket;
+
+    private void handleIncomingConnections()  {
         // Need to take in a filename and a chunk number and then start sending.
+        String fileRequested;
+        try{
+            clientServerSocket = new Socket(InetAddress.getLocalHost(), SERVER_PORT);
+            while(true) {
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientServerSocket.getInputStream()));
+                fileRequested = inFromClient.readLine();
+                
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e);
+            System.exit(1);
+        }
+
     }
+
 
     public void start() {
         handleIncomingConnections();
