3190efa1376f651a02cf68cd44630d7e836e8e2c
Unknown <e0032176@u.nus.edu>
2018-10-13 22:53:23 +0800

    [2nd PR] Implementation of Inform&Update and fix one bug
    Implementation of Inform & update on some assumptions (subject for discussion):
    
    Assuming user only advertise/upload when he download an entire file, so instead of sending one list of chunk numbers, I just send how many chunks in total to the tracker.
    chunk size set to 1200 (I not very sure about this)
    User need to give the directory of the folder which the file reside before he indicate the file name. (I am thinking maybe we can allow our user at the start of using the program he will need to specific where his file will be uploading from/downloading to(only one folder allowed)
    assuming chunk number starts with 1.
    Regarding the bug i fixed:
    
    I added input.nextLine() under the option = input.nextInt() because if I didnt do that, the "\n" character will not be consumed by previous nextInt() and the program will not be able to capture the extra parameter needed for the options(for example: file name etc).

--- src/P2PClient/P2PClientUser.java
+++ src/P2PClient/P2PClientUser.java
@@ -1,15 +1,20 @@
 package P2PClient;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class P2PClientUser extends Thread {
+
     public static final int SERVER_PORT = 8888;
     public static final String LIST_COMMAND = "LIST";
     public static final String QUERY_COMMAND = "FIND";
     public static final String EXIT_COMMAND = "EXIT";
+    public static final String INFORM_COMMAND = "INFORM";
+
+    public static final int CHUNK_SIZE = 1200; //following MTU byte size of 1500, to play safe make it slightly lesser
 
     private Socket clientRequestSocket;
     private PrintWriter toServer;
@@ -27,11 +32,11 @@ public class P2PClientUser extends Thread {
              while(true) {
                  int option;
                 displayMenu();
-                System.out.println("Please enter your option (1-5): ");
 
+                System.out.println("Please enter your option (1-5): ");
                 // Might want to catch error and warn user to input correctly before looping back.
                 option = input.nextInt();
-
+                input.nextLine();
                 switch(option) {
                     case 1:
                         requestForListOfFiles();
@@ -44,6 +49,7 @@ public class P2PClientUser extends Thread {
                         break;
                     case 4:
                         // Inform method
+                        informAndUpdate();
                         break;
                     case 5:
                         exitFromProgram();
@@ -54,8 +60,6 @@ public class P2PClientUser extends Thread {
 
 
              }
-
-
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println(e);
@@ -77,6 +81,62 @@ public class P2PClientUser extends Thread {
         System.out.println("***************************************");
         System.out.println("***************************************");
         System.out.println();
+    }
+
+    private void informAndUpdate() {
+
+        try {
+
+            //get the local IP address
+            String localAddress = InetAddress.getLocalHost().getHostAddress();
+
+            //Get the directory of the folder
+            //(I am thinking in the future at the start of the application we prompt this step to the user and let user
+            //specify which folder he is using to download/upload file, assuming we only allows him to use one folder)
+            System.out.println("Please enter the directory of the folder where the file reside: ");
+            input.nextLine(); //consume the "\n" previously not captured by nextInt();
+
+            String fileDirectory = input.nextLine().trim();
+            File advertisingFolder = new File(fileDirectory);
+            //check whether if the directory indicated is indeed a directory and exits
+            if (!advertisingFolder.exists() || !advertisingFolder.isDirectory()) {
+                System.out.println("Folder not found, please re-enter valid folder directory: ");
+                return;
+            }
+
+            //Obtaining file name
+            System.out.println("Please provide the file name you wish to advertise ");
+            String fileName = input.next().trim();
+            File advertisingFile = new File(advertisingFolder + "\\" + fileName);
+            //check whether if the indicated file exists and whether if it is a file
+            if (!advertisingFile.exists() || !advertisingFile.isFile()) {
+                System.out.println("File not found, please re-enter valid file name of existing file in this folder: ");
+                return;
+            }
+
+            //calculate number of chunk
+            int fileSize = (int) advertisingFolder.length();
+            int numOfChunks;
+            if (fileSize % CHUNK_SIZE > 0) {
+                numOfChunks = fileSize / CHUNK_SIZE + 1;
+            } else {
+                numOfChunks = fileSize / CHUNK_SIZE;
+            }
+
+            String request = INFORM_COMMAND + " " + localAddress + " " + fileName + " " + numOfChunks;
+            System.out.println(request);
+            toServer.println(request);
+            toServer.flush();
+            String replyFromServer = fromServer.nextLine();
+            System.out.println(replyFromServer);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println(e);
+            System.exit(1);
+        }
+
     }
 
     private void exitFromProgram() {
